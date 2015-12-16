830b2e2a6e214890761eac7676382eecfd5ac2d4
Josh Quintus <josh@dotalign.com>
2015-12-16 10:51:09 -0500

    Up the version number for the chocolatey installer

--- ColorCat/Chocolatey/Tools/chocolateyInstall.ps1
+++ ColorCat/Chocolatey/Tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'ColorCat.portable' # arbitrary name for the package, used in messages
-$url ="https://github.com/MasterDevs/ColorCat/releases/download/v0.0.2/bin.zip"
+$url ="https://github.com/MasterDevs/ColorCat/releases/download/v0.0.3/bin.zip"
 
 try 
 {
