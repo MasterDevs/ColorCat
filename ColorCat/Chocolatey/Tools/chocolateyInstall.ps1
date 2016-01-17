d464e6c6ccc24baa653256750ea85a7d6f0730bd
Josh <josh.quintus@gmail.com>
2016-01-17 16:47:19 -0500

    Implement suggestions from chocolatey review
    * Added suggested properties to Nuspec
    * Remove try/catch in the installer scripts
    * Remove Write-ChocolateyFailure
    * Update version number in install url

--- ColorCat/Chocolatey/Tools/chocolateyInstall.ps1
+++ ColorCat/Chocolatey/Tools/chocolateyInstall.ps1
@@ -1,17 +1,9 @@
-﻿$packageName = 'ColorCat.portable' # arbitrary name for the package, used in messages
-$url ="https://github.com/MasterDevs/ColorCat/releases/download/v0.0.2/bin.zip"
+﻿$packageName = 'ColorCat.portable' # name for the package, used in messages
+$url ="https://github.com/MasterDevs/ColorCat/releases/download/v0.0.4/bin.zip"
 
-try 
-{
-  $installDir = Join-Path $env:AllUsersProfile "$packageName"
-  Write-Host "Adding `'$installDir`' to the path and the current shell path"
-  Install-ChocolateyPath "$installDir"
-  $env:Path = "$($env:Path);$installDir"
+$installDir = Join-Path $env:AllUsersProfile "$packageName"
+Write-Host "Adding `'$installDir`' to the path and the current shell path"
+Install-ChocolateyPath "$installDir"
+$env:Path = "$($env:Path);$installDir"
 
-  Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
-} 
-catch 
-{
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw
-}
+Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
--- ColorCat/Chocolatey/Tools/chocolateyUninstall.ps1
+++ ColorCat/Chocolatey/Tools/chocolateyUninstall.ps1
@@ -1,14 +1,6 @@
-﻿$packageName = 'ColorCat.portable' # arbitrary name for the package, used in messages
+﻿$packageName = 'ColorCat.portable' # name for the package, used in messages
 $zipName = "bin.zip"
 
-try 
-{
-  $installDir = Join-Path $env:AllUsersProfile "$packageName"
-  Uninstall-ChocolateyZipPackage "$packageName" "$zipName" 
-  Remove-Item -Recurse -Force "$installDir"
-} 
-catch 
-{
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw
-}
+$installDir = Join-Path $env:AllUsersProfile "$packageName"
+Uninstall-ChocolateyZipPackage "$packageName" "$zipName" 
+Remove-Item -Recurse -Force "$installDir"
